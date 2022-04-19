e19c768a5dbf921741b947b0ccca75541996b130
ARPRI <arpriansah@gmail.com>
2022-04-19 11:24:32 +0700

    .

--- resources/views/homeafterlogin.blade.php
+++ resources/views/homeafterlogin.blade.php
@@ -25,11 +25,11 @@
             <div class="offcanvas-body">
               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                   <li class="nav-item">
-                    <a class="nav-link active" href="{{route('destinasiuser')}}">Destinasi</a>
+                    <a class="nav-link active" href="/destinasi">Destinasi</a>
                   <li class="nav-item">
                     <a class="nav-link active" href="{{route('logout')}}">Sign-Out</a>
                   <li class="nav-item">
-                    <a class="nav-link active" href="{{route('destinasiadmin')}}">Edit</a>
+                    <a class="nav-link active" href="/editdestinasi">Edit</a>
                 </ul>
             </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
